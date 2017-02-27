4fbb285dd37479580514bea39739e10c467b106c
med-joj <med-joj@BMCL7030>
2017-02-27 11:45:04 +0100

    Add project files.

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,113 @@
+#include "FBullCowGame.h"
+#include <map>
+#define TMap std::map
+
+using int32 = int;
+
+FBullCowGame::FBullCowGame() { Reset(); }
+
+int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
+int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
+bool FBullCowGame::IsGameWon() const { return bGameIsWon; }
+
+void FBullCowGame::Reset()
+{
+	constexpr int32 MAX_TRIES = 3;
+	const FString HIDDEN_WORD = "planet";
+
+	MyMaxTries = MAX_TRIES;
+	MyHiddenWord = HIDDEN_WORD;
+	MyCurrentTry = 1;
+	bGameIsWon = false;
+
+	return;
+}
+
+EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const 
+{
+	if (!IsIsogram(Guess)) // if the guess isn't an isogram
+	{
+		return EGuessStatus::Not_isogram;
+	}
+	else if (false)
+	{
+		return EGuessStatus::Not_Lowercase; // TODO write function
+	}
+	else if (Guess.length() != GetHiddenWordLength()) // if the guess length is wrong
+	{
+		return EGuessStatus::Wrong_Length;
+	}
+	else
+	{
+		return EGuessStatus::OK;
+	}
+}
+
+
+//receives a VALID guess, increments turn, and returns count
+FBullCowCount FBullCowGame::SubmitValidGess(FString Guess)
+{
+	MyCurrentTry++;
+	FBullCowCount BullCowCount;
+	int32 WordLength = MyHiddenWord.length();	// assuming same length as guess
+
+	// loop through all letters in the hidden word
+	for (int32 MHWChar = 0; MHWChar < WordLength; MHWChar++)
+	{
+		// compare letters against the guess
+		for (int32 GChar = 0; GChar < WordLength; GChar++) 
+		{
+			// if they match then
+			if (Guess[GChar] == MyHiddenWord[MHWChar])
+			{
+				if (MHWChar == GChar)
+				{
+					// if they're in the same place
+					BullCowCount.Bulls++;	// increment bulls
+				}
+				else
+				{
+					BullCowCount.Cows++;	// must be a cow
+				}
+			}
+		}
+	}
+
+	if (BullCowCount.Bulls == WordLength) 
+	{
+		bGameIsWon = true;
+	}
+	else
+	{
+		bGameIsWon = false;
+	}
+
+	return BullCowCount;
+
+}
+
+
+bool FBullCowGame::IsIsogram(FString Word) const
+{
+	// treat 0 and 1 letter words as isograms
+	if (Word.length() <= 1) { return true; }
+
+	// setup our map
+	TMap<char, bool> LetterSeen;
+
+	for (char Letter : Word)	// for all letters of the word.
+	{
+		// loop through all the letters of the word
+
+		// if the letter is in the map
+		// we do NOT have an isogram
+		// otherwise
+		// add the letter to the map as seen
+	}
+
+	
+
+
+	return true; // for example in cases where /0 is entered
+}
